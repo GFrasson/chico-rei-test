d6e31442f9a09c5ea2f5dad6cf3c1144cd33a07f
Gabriel Frasson <bielfc27@gmail.com>
2023-05-13 16:40:32 -0300

    Use global styles

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,10 @@
 import './bootstrap';
-import router from './router'
-
 import {createApp} from 'vue';
 
+import router from './router'
 import App from './App.vue';
+
+import '../css/main.css';
 
 const app = createApp(App);
 
