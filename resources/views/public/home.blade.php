79fdd2455795da07cf9902e06787c7c282bf805a
Gabriel Frasson <bielfc27@gmail.com>
2023-05-13 16:40:55 -0300

    Add roboto font

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -7,8 +7,8 @@
         <title>Chico Rei</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 
         <!-- Styles -->
         @vite('resources/css/app.css')
