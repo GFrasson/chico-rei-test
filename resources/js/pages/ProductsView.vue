158c58102207ae912c4618a0d765d140913e6649
Gabriel Frasson <bielfc27@gmail.com>
2023-05-14 23:26:13 -0300

    Products filters select

--- resources/js/pages/ProductsView.vue
+++ resources/js/pages/ProductsView.vue
@@ -1,4 +1,5 @@
 <script lang="ts">
+import { PhSlidersHorizontal } from '@phosphor-icons/vue';
 import ProductCard from '../components/ProductCard.vue';
 import Pagination from '../components/Pagination.vue';
 import { listProducts } from '../services/productsService';
@@ -13,18 +14,27 @@ interface Product {
   img_thumb?: string;
 }
 
+interface Filters {
+  types: string[];
+}
+
 export default {
   name: "ProductsView",
   components: {
     ProductCard,
     Pagination,
+    PhSlidersHorizontal
   },
   async mounted() {
-    this.products = await listProducts(this.currentPage);
+    const { products, filters } = await listProducts(this.currentPage);
+    this.products = products;
+    this.filters = filters;
   },
   data() {
     return {
       products: [] as Product[],
+      filters: {} as Filters,
+      typeSelected: null,
       currentPage: 1,
       totalPages: 7
     }
@@ -32,18 +42,43 @@ export default {
   methods: {
     async goToPage(page: number) {
       this.currentPage = page;
-      this.products = await listProducts(this.currentPage);
+      const { products } = await listProducts(this.currentPage);
+      this.products = products;
 
       window.scrollTo({
         top: 0,
         behavior: 'smooth'
       });
+    },
+    handleTypeFilterChange(event) {
+      this.typeSelected = event.target.value;
     }
   }
 }
 </script>
 
 <template>
+  <div class="filters">
+    <PhSlidersHorizontal :size="32" />
+    
+    <select
+      class="type"
+      name="type"
+      id="type"
+      @select="handleTypeFilterChange($event)"
+    >
+      <option value="" disabled selected>TIPO</option>
+      <option
+        v-if="filters.hasOwnProperty('types')"
+        v-for="filterType of filters.types"
+        :value="filterType"
+        :selected="typeSelected === filterType"
+      >
+        {{ filterType }}
+      </option>
+    </select>
+  </div>
+
   <main>
     <ProductCard
       v-if="products.length > 0"
@@ -58,6 +93,7 @@ export default {
     />
     <h2 v-else>Carregando os produtos...</h2>
   </main>
+
   <footer>
     <Pagination
       :total-pages="totalPages"
@@ -68,9 +104,35 @@ export default {
 </template>
 
 <style scoped lang="less">
-main {
+.filters {
+  margin: 6rem auto 0 auto;
   max-width: 90%;
-  margin: 6rem auto 2rem;
+
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 2rem;
+  
+  & > select {
+    background-color: transparent;
+    border: 1px solid var(--black);
+    padding: 0.25rem 0.5rem;
+    width: 12rem;
+    font-size: 0.9rem;
+    cursor: pointer;
+    transition: 0.2s all;
+
+    &:hover {
+      background-color: var(--color-background-dark);
+      color: var(--white);
+      transition: 0.2s all;
+    }
+  }
+}
+
+main {
+  max-width: 95%;
+  margin: 1rem auto 2rem;
   display: flex;
   justify-content: center;
   align-items: flex-start;
